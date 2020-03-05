e85c61571fee4a012bcb13f06adc7f53243f186c
lively-bundle-0725 <tbandachit@gmail.com>
2020-03-05 02:43:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,9 +15,12 @@ end
 
 def monopoly_with_third_tier
     {
-      :railroads => { :rent_in_dollars = { :one_piece_owned = 0, 
-      one_piece_owned
-        :
+      :railroads => { :rent_in_dollars = 
+      { 
+      :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
       }
     }
 end
